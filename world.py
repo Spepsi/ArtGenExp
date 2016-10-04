44878dddf1064bc5155e49380ec29cb3c5d3ede9
KevinBienvenu <bienvenu.kevin@sfr.fr>
2016-10-04 23:44:56 +0200

    c

--- world.py
+++ world.py
@@ -8,8 +8,8 @@ sizeY = 100
 nb_humans_start = 100
 
 proba_food = 0.05
-proba_water = 0.05
-proba_rock = 0.05
+proba_water = 0.01
+proba_rock = 0.2
 
 debug = True
 
@@ -39,13 +39,49 @@ class World:
 			self.create_human(Human(self,self.idx))
 			self.idx+=1
 
-		# water
+		# rock et water de base
 		for i in range(1,sizeX-1):
 			for j in range(1,sizeY-1):
-				if np.random.random()<proba_rock:
+				d = 1.0-4.0*(min(i,sizeX-i)*min(i,sizeX-i)+min(j,sizeY-j)*min(j,sizeY-j))/(sizeX*sizeY)
+				if np.random.random()<d*proba_rock:
 					self.board['rock'][i,j] = 1
-				if np.random.random()<proba_water:
+				if np.random.random()<(1.0-d)*proba_water:
 					self.board['water'][i,j] = 1
+		# propagate water into river
+		proba_water_propagate = 0.13
+		for n in range(20):
+			for i in range(1,sizeX-1):
+				for j in range(1,sizeY-1):
+					if self.is_water_possible(i,j):
+						p = 0
+						for delta in [[1,0],[-1,0],[0,1],[0,-1],[1,1],[-1,-1],[-1,1],[1,-1]]:
+							i2,j2 = i,j
+							for edge in range(5):
+								i2 += delta[0]
+								j2 += delta[1]
+								if i2>=0 and j2>=0 and i2<sizeX and j2<sizeY and self.board["water"][i2,j2]>0:
+									p += proba_water_propagate
+								else:
+									break
+							if edge>0:
+								p-= proba_water_propagate/0.9
+						if np.random.random()<p:
+							self.board["water"][i,j] += 1
+		# feed the rivers
+		for n in range(2):
+			for i in range(1,sizeX-1):
+				for j in range(1,sizeY-1):
+					if self.is_water_possible(i,j):
+						p = 0
+						for delta in [[1,0],[-1,0],[0,1],[0,-1],[1,1],[-1,-1],[-1,1],[1,-1]]:
+							i2,j2 = i,j
+							i2 += delta[0]
+							j2 += delta[1]
+							if i2>=0 and j2>=0 and i2<sizeX and j2<sizeY and self.board["water"][i2,j2]>0:
+								p += proba_water_propagate/2.0
+							if np.random.random()<p:
+								self.board["water"][i,j] += 1
+
 		for i in range(sizeX):
 			self.board['water'][i,0] = 1
 			self.board['water'][i,sizeY-1] = 1
@@ -74,7 +110,7 @@ class World:
 			if self.is_food_possible(i,j):
 				self.board["food"][i,j]+=1
 		# Propagate food
-		proba_food_propagate = 0.2
+		proba_food_propagate = 0.05
 		proba_food_growth = 0.2
 		for i in range(1,self.sizeX-1):
 			for j in range(1,self.sizeY-1):
@@ -86,6 +122,10 @@ class World:
 					for i2,j2 in [[i-1,j],[i+1,j],[i,j-1],[i,j+1]]:
 						if self.board["food"][i2,j2]>0:
 							p += proba_food_propagate
+					if p>0:
+						for i2,j2 in [[i-1,j],[i+1,j],[i,j-1],[i,j+1]]:
+							if self.board["water"][i2,j2]>0:
+								p += proba_food_propagate/2
 					if np.random.random()<p:
 						self.board["food"][i,j] += 1
 		to_remove = []
@@ -107,4 +147,8 @@ class World:
 
 	def is_food_possible(self,i,j):
 		return self.board["rock"][i,j]==0 and self.board["water"][i,j]==0
+	def is_water_possible(self,i,j):
+		return self.board["rock"][i,j]==0
+
+
 
