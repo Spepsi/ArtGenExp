90401f7cdf191b84abfe4564d5f380a9bc6fb56c
KevinBienvenu <bienvenu.kevin@sfr.fr>
2016-10-04 20:24:12 +0200

    ajout nterface

---
+++ interface.py
@@ -0,0 +1,79 @@
+import numpy as np
+import random
+import pygame
+
+resX = 640
+resY = 480
+debug_lignes = True
+
+# Color
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
+YELLOW =(255, 255, 255)
+CYAN   =(  0, 255,   0)
+MAJENTA=(255,   0, 255)
+DRKGRAY=( 50,  50,  50)
+
+dicColor = {"water":BLUE, 
+			"food":GREEN, 
+			"rock":DRKGRAY,
+			"pheromones":CYAN,
+			"humans":RED}
+
+def main():
+	pygame.init()
+	fenetre = pygame.display.set_mode((resX, resY))
+	continuer = True
+	clock = pygame.time.Clock()
+	while continuer:
+		draw(fenetre)
+		update()
+		clock.tick(10)
+
+
+def draw(fenetre):
+	# TODO : get the real world
+	board = None
+	fenetre.fill(BLACK)
+	if board is not None:
+		sizeCaseX = 1.0 * resX / len(board["water"])
+		sizeCaseY = 1.0 * resY / len(board["water"][0])
+		# draw water, rock, food
+		for key in ["water","food","rock"]:
+			for i in range(len(board[key])):
+				for j in range(len(board[key][0])):
+					if board[key][i,j]>0:
+						pygame.draw.rect(fenetre, 
+										dicColor[key], 
+										[i*sizeCaseX,j*sizeCaseY,sizeCaseX,sizeCaseY])
+		# draw humans and pheromones
+		for key in ["pheromones","humans"]:
+			for i in range(len(board[key])):
+				for j in range(len(board[key][0])):
+					if board[key][i,j]>0:
+						for k in range(board[key][i,j]):
+							xrand = i*sizeCaseX + random.random()*sizeCaseX
+							yrand = j*sizeCaseY + random.random()*sizeCaseY
+							pygame.draw.rect(fenetre,dicColor[key],[xrand,yrand,1,1])
+	else:
+		nbCaseX = 100
+		nbCaseY = 50
+		sizeCaseX = 1.0 * resX / nbCaseX
+		sizeCaseY = 1.0 * resY / nbCaseY
+
+	# DEBUG : afficher les lignes
+	if debug_lignes:
+		for i in range(nbCaseX):
+			pygame.draw.line(fenetre, WHITE, [i*sizeCaseX,0], [i*sizeCaseX,resY],1)
+		for j in range(nbCaseY):
+			pygame.draw.line(fenetre, WHITE, [0,j*sizeCaseY], [resX,j*sizeCaseY],1)
+	pygame.display.flip()
+
+def update():
+	pass
+
+
+main()
